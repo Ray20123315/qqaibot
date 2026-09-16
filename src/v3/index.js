fd4c22f6dbe06ddd574598e3c6436e0ae910c51a
raychen <150588882+Ray20123315@users.noreply.github.com>
2026-09-16 19:42:04 +0800

    feat(v3): export plugin scheduler surface

--- src/v3/index.js
+++ src/v3/index.js
@@ -2,5 +2,6 @@ export * from "./message/index.js";
 export * from "./media/index.js";
 export * from "./ai/index.js";
 export * from "./host/index.js";
+export * from "./scheduler/index.js";
 export * from "./shadow/index.js";
 export * from "./canary/index.js";
