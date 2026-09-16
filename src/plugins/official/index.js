ac0ecd36f4272e0756d0c3a38dd77e24100bee14
raychen <150588882+Ray20123315@users.noreply.github.com>
2026-09-16 22:54:13 +0800

    feat(v3): export official Bilibili live plugin factory

--- src/plugins/official/index.js
+++ src/plugins/official/index.js
@@ -1,6 +1,7 @@
+import { createBilibiliLivePlugin } from "./bilibili-live.js";
 import { helloPlugin } from "./hello.js";
 import { shadowDiagnosticsPlugin } from "./shadow-diagnostics.js";
 
 const OFFICIAL_BUNDLED_PLUGINS = Object.freeze([helloPlugin]);
 
-export { OFFICIAL_BUNDLED_PLUGINS, helloPlugin, shadowDiagnosticsPlugin };
+export { OFFICIAL_BUNDLED_PLUGINS, createBilibiliLivePlugin, helloPlugin, shadowDiagnosticsPlugin };
