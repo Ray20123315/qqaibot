6fcf2db19e771852b281eb7324d772691fdfae9b
raychen <150588882+Ray20123315@users.noreply.github.com>
2026-09-25 04:42:17 +0800

    feat(v3): register member speech analysis plugin

--- src/v3/runtime/runtime.js
+++ src/v3/runtime/runtime.js
@@ -1,4 +1,4 @@
-import { createActivityPlugin, createBilibiliLivePlugin, createPollPlugin, entertainmentPlugin } from "../../plugins/official/index.js";
+import { createActivityPlugin, createBilibiliLivePlugin, createPollPlugin, entertainmentPlugin, memberSpeechAnalysisPlugin } from "../../plugins/official/index.js";
 import { createV3HostAdapter } from "../host/adapter.js";
 import { buildV3PublicStatus, v3PublicStatusResponse } from "../public/status.js";
 
@@ -15,6 +15,7 @@ function createOfficialV3Plugins(options = {}) {
   if (source.entertainment !== false) plugins.push(entertainmentPlugin);
   if (source.activity !== false) plugins.push(createActivityPlugin({ adminUserIds }));
   if (source.poll !== false) plugins.push(createPollPlugin({ adminUserIds }));
+  if (source.memberSpeechAnalysis !== false) plugins.push(memberSpeechAnalysisPlugin);
   if (source.bilibili && source.bilibili !== false) {
     if (typeof source.bilibili !== "object") throw new Error("V3_BILIBILI_OPTIONS_INVALID");
     plugins.push(createBilibiliLivePlugin(source.bilibili));
