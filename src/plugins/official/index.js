787b6549e21e89dfae9c8df23a056c2ce270707f
raychen <150588882+Ray20123315@users.noreply.github.com>
2026-09-25 04:42:14 +0800

    feat(v3): export member speech analysis plugin

--- src/plugins/official/index.js
+++ src/plugins/official/index.js
@@ -1,3 +1,4 @@
+import { memberSpeechAnalysisPlugin } from "./member-speech-analysis.js";
 import { createActivityPlugin } from "./activity.js";
 import { createPollPlugin } from "./poll.js";
 import { entertainmentPlugin } from "./entertainment.js";
@@ -7,4 +8,4 @@ import { shadowDiagnosticsPlugin } from "./shadow-diagnostics.js";
 
 const OFFICIAL_BUNDLED_PLUGINS = Object.freeze([helloPlugin]);
 
-export { OFFICIAL_BUNDLED_PLUGINS, createActivityPlugin, createBilibiliLivePlugin, createPollPlugin, entertainmentPlugin, helloPlugin, shadowDiagnosticsPlugin };
+export { OFFICIAL_BUNDLED_PLUGINS, createActivityPlugin, createBilibiliLivePlugin, createPollPlugin, entertainmentPlugin, helloPlugin, memberSpeechAnalysisPlugin, shadowDiagnosticsPlugin };
