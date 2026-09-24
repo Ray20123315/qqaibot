9d86a333bdbe813890ac09b7757ce5f98432fc2f
raychen <150588882+Ray20123315@users.noreply.github.com>
2026-09-25 04:37:52 +0800

    feat(v3): export entertainment plugin

--- src/plugins/official/index.js
+++ src/plugins/official/index.js
@@ -1,7 +1,8 @@
+import { entertainmentPlugin } from "./entertainment.js";
 import { createBilibiliLivePlugin } from "./bilibili-live.js";
 import { helloPlugin } from "./hello.js";
 import { shadowDiagnosticsPlugin } from "./shadow-diagnostics.js";
 
 const OFFICIAL_BUNDLED_PLUGINS = Object.freeze([helloPlugin]);
 
-export { OFFICIAL_BUNDLED_PLUGINS, createBilibiliLivePlugin, helloPlugin, shadowDiagnosticsPlugin };
+export { OFFICIAL_BUNDLED_PLUGINS, createBilibiliLivePlugin, entertainmentPlugin, helloPlugin, shadowDiagnosticsPlugin };
